dceddb5678a1fbd6fbfefd033161e256a1c7918a
Manuel Rocha <manueltemidorocha@gmail.com>
2020-01-24 21:09:49 +0000

    All done excluding super bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -63,12 +63,8 @@ class War {
     let randViki = Math.floor(Math.random() * this.vikingArmy.length);
 
     let attackDamage = this.saxonArmy[randSaxon].receiveDamage(this.vikingArmy[randViki].strength);
-    if (this.saxonArmy.health <= attackDamage) {
-      this.saxonArmy.filter((x, i) => {
-        if (this.saxonArmy.health <= 0) {
-          return (this.saxonArmy[randSaxon].length -= 1);
-        }
-      });
+    if (this.saxonArmy[randSaxon].health <= 0) {
+      this.saxonArmy.splice(randSaxon, 1);
     }
     return attackDamage;
   }
@@ -78,8 +74,16 @@ class War {
     let randViki = Math.floor(Math.random() * this.vikingArmy.length);
 
     let attackDamage = this.vikingArmy[randViki].receiveDamage(this.saxonArmy[randSaxon].strength);
-
+    if (this.vikingArmy[randViki].health <= 0) {
+      this.vikingArmy.splice(randViki, 1);
+    }
     return attackDamage;
   }
-  showStatus() {}
+  showStatus() {
+    if (this.saxonArmy.length === this.vikingArmy.length)
+      return 'Vikings and Saxons are still in the thick of battle.';
+    else if (this.saxonArmy.length <= 0) return 'Vikings have won the war of the century!';
+    else if (this.vikingArmy.length <= 0)
+      return 'Saxons have fought for their lives and survived another day...';
+  }
 }
