516d5616d5a0e13cc734e4e0305ceaf69dd0b2e3
unknown <ricardotomaz027@gmail.com>
2020-01-25 18:05:30 +0000

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -46,8 +46,10 @@ class Saxon extends Soldier {
 
 // War
 class War {
-  vikingArmy = [];
-  saxonArmy = [];
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
 
   addViking(Viking) {
     this.vikingArmy.push(Viking);
